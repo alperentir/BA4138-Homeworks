bc5ba9e123b89c35f192a63288c19df6b29b9f96
alperentir <56046705+alperentir@users.noreply.github.com>
2019-10-15 21:59:49 +0300

    Add files via upload

---
+++ HW3-AlperenTIR.py
@@ -0,0 +1,9 @@
+age = int(input("Please enter an age. " )) 
+if 10 <= age <= 19:
+    print ("The person is an adolescent")
+if age > 19:
+    print ("The person is an adult")
+if age <= 1:
+    print ("The person is an infant")
+if 1 < age < 10:
+    print ("The person is a child")
